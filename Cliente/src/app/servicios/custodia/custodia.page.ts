c1e1bcc0777b1043bd21e9c3415f615e27081937
Adagmari <saintria@espol.edu.ec>
2021-10-27 00:32:51 -0500

    pantallas de los servicios del cliente
    Creación de las pantallas de los servicios de transporte, custodia, chofer y guardia con diseño, para acceder por el momento localmente se usa http://localhost:8100/servicios/n/chofer
    se reemplaza chofer por el servicio que se desea ver.

--- Cliente/src/app/servicios/custodia/custodia.page.ts
+++ Cliente/src/app/servicios/custodia/custodia.page.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custodia.page.scss'],
 })
 export class CustodiaPage implements OnInit {
+  currentNumber = 0;
+
+  increment() {
+    this.currentNumber++;
+ }
+ 
+ decrement() {
+    if (this.currentNumber > 0) {
+       this.currentNumber--;
+    }
+ }
 
   constructor() { }
 
