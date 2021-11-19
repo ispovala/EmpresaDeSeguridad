374f6695b8d935bb6d6b0a9a08bb240a379d5a7a
Hayleen <haivcarr@espol.edu.ec>
2021-11-19 14:35:23 -0500

    forms con validaciones

--- Cliente/src/app/editarperfil/editarperfil.module.ts
+++ Cliente/src/app/editarperfil/editarperfil.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,12 +7,15 @@ import { EditarperfilPageRoutingModule } from './editarperfil-routing.module';
 
 import { EditarperfilPage } from './editarperfil.page';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EditarperfilPageRoutingModule
+    EditarperfilPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [EditarperfilPage]
 })
--- Cliente/src/app/editarperfil/editarperfil.page.ts
+++ Cliente/src/app/editarperfil/editarperfil.page.ts
@@ -1,16 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 @Component({
   selector: 'app-editarperfil',
   templateUrl: './editarperfil.page.html',
   styleUrls: ['./editarperfil.page.scss'],
 })
 export class EditarperfilPage implements OnInit {
+  ionicForm: FormGroup;
+  defaultDate = "1987-06-30";
+  isSubmitted = false;
 
-  constructor(public alertController: AlertController) { }
+  constructor(public formBuilder: FormBuilder, public alertController: AlertController) { }
 
   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      dob: [this.defaultDate],
+      mobile: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      cedula: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      direccion: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z-0-9\.]*')]],
+    })
   }
+
+  getDate(e) {
+    let date = new Date(e.target.value).toISOString().substring(0, 10);
+    this.ionicForm.get('fechanacimiento').setValue(date, {
+       onlyself: true
+    })
+ }
+
+ get errorControl() {
+  return this.ionicForm.controls;
+}
+
+submitForm() {
+  this.isSubmitted = true;
+  if (!this.ionicForm.valid) {
+    console.log('Please provide all the required values!')
+    return false;
+  } else {
+    console.log(this.ionicForm.value)
+  }
+}
 
   async presentAlertConfirm() {
     const alert = await this.alertController.create({
@@ -38,4 +73,5 @@ export class EditarperfilPage implements OnInit {
     console.log(result);
   }
 
+ 
 }
