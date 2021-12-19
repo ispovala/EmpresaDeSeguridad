d4822cf0daaaa6103936fd86c1c98651ea1506ac
Hayleen <haivcarr@espol.edu.ec>
2021-12-19 16:55:22 -0500

    editar perfil fin

--- Cliente/src/app/editarperfil/editarperfil.page.ts
+++ Cliente/src/app/editarperfil/editarperfil.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators, ValidationErrors, ValidatorFn } from "@angular/forms";
+import * as validarcedula from 'src/scripts/validarcedula.js';
+import {validarCedulaAlg} from 'src/app/editarperfil/cedula.validator';
 
 @Component({
   selector: 'app-editarperfil',
@@ -21,7 +23,11 @@ export class EditarperfilPage implements OnInit {
   constructor(private navCtrl: NavController, public formBuilder: FormBuilder, public alertController: AlertController) { }
 
   ngOnInit() {
-    this.ionicForm = this.formBuilder.group({
+    this.initForm();
+  }
+
+  initForm(){
+      this.ionicForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]*')]],
       lastname: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]*')]],
       email: ['', [Validators.required, Validators.pattern('[a-z0-9_-]+@[a-z0-9]+[.{1}][a-z]+([.{1}][a-z]+)?')]],
@@ -29,8 +35,17 @@ export class EditarperfilPage implements OnInit {
       mobile: ['', [Validators.required, Validators.pattern('^09[0-9]+$') ]],
       cedula: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
       direccion: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z0-9- .#]*')]],
-    })
+    },
+    {
+      validators: validarCedulaAlg
+    });
   }
+
+  ingresoCedula(){
+    return this.ionicForm.hasError('CedulaNoValida') && this.ionicForm.get('cedula').dirty;
+  }
+
+  
 
   getDate(e) {
     let date = new Date(e.target.value).toISOString().substring(0, 10);
@@ -110,54 +125,37 @@ submitForm() {
     this.navCtrl.navigateForward("/perfil");
     this.ionicForm.reset()
   }
-  /*
-  validadorDeCedula() {
-    let cedulaCorrecta = false;
-    console.log("entró al validador")
-    let cedula=this.ionicForm.controls['cedula'].value
-    console.log(cedula.length)
-    if (cedula.length == 10)
-    {   
-        console.log("igual a 10")
-        let tercerDigito = parseInt(cedula.substring(2, 3));
-        if (tercerDigito < 6) {
-          console.log("menor a 6")
-        
-            // El ultimo digito se lo considera dígito verificador
-            let coefValCedula = [2, 1, 2, 1, 2, 1, 2, 1, 2];       
-            let verificador = parseInt(cedula.substring(9, 10));
-            let suma:number = 0;
-            let digito:number = 0;
-            for (let i = 0; i < (cedula.length - 1); i++) {
-                digito = parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];      
-                suma += ((parseInt((digito % 10)+'') + (parseInt((digito / 10)+''))));
-          //      console.log(suma+" suma"+coefValCedula[i]); 
-            }
-            
-            suma= Math.round(suma);
-          
-          //  console.log(verificador);
-          //  console.log(suma);
-          //  console.log(digito);
+
+  validacionCed(){
+    let cedula = this.ionicForm.get("cedula").value;
+    //console.log(cedula);
+    if (typeof(cedula) == 'string' && cedula.length == 10 && /^\d+$/.test(cedula)) {
+      var digitos = cedula.split('').map(Number);
+      var codigo_provincia = digitos[0] * 10 + digitos[1];
   
-            if ((Math.round(suma % 10) == 0) && (Math.round(suma % 10)== verificador)) {
-                cedulaCorrecta = true;
-            } else if ((10 - (Math.round(suma % 10))) == verificador) {
-                cedulaCorrecta = true;
-            } else {
-                cedulaCorrecta = false;
-            }
-        } else {
-            cedulaCorrecta = false;
-        }
-    } else {
-        cedulaCorrecta = false;
+      //if (codigo_provincia >= 1 && (codigo_provincia <= 24 || codigo_provincia == 30) && digitos[2] < 6) {
+  
+      if (codigo_provincia >= 1 && (codigo_provincia <= 24 || codigo_provincia == 30)) {
+        var digito_verificador = digitos.pop();
+  
+        var digito_calculado = digitos.reduce(
+          function (valorPrevio, valorActual, indice) {
+            //console.log(typeof valorPrevio);
+            return valorPrevio - (valorActual * (2 - indice % 2)) % 9 - +(valorActual == 9) * 9;
+          }, 1000) % 10;
+        //console.log(digito_calculado === digito_verificador);
+        return digito_calculado === digito_verificador;
+    }else{
+      alert("Ingrese una cédula válida");
+      return false;
     }
-  
-  
-  this.validador= cedulaCorrecta;
-  
-    
   }
- */
+   
+  
+  }
+  
+  
+
+
 }
+
