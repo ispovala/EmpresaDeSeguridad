07469e9b0249359a478dd2912e14a98dc23bbbc7
Paul Douglas Bustos Mite <paudobus@espol.edu.ec>
2022-01-26 18:39:23 -0500

    Settings updated

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from server import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +17,7 @@ urlpatterns = [
     url(r'^', include('seguridad.urls')),
 
     # Administracion
-    url(r'login/', TemplateView.as_view(template_name="home.html"), name="home"),
+    url(r'^(?P<path>.*)/$', views.index),
+    # url(r'login/', TemplateView.as_view(template_name="home.html"), name="home"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
