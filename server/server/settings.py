763744854652b46917150c433c669ecab117270f
Paul Douglas Bustos Mite <paudobus@espol.edu.ec>
2022-01-26 12:17:52 -0500

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -1,6 +1,9 @@
 import datetime
 import os
+import mimetypes
 from pathlib import Path
+
+mimetypes.add_type("text/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
